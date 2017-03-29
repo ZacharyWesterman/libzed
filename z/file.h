e535b4d17f0fd42aa6e4ff5ca287c081df257838
Zachary Westerman <zacharywesterman@yahoo.com>
2017-03-29 18:12:27 -0500

    added z::file header

---
+++ z/file.h
@@ -0,0 +1,23 @@
+/**
+ * File:            file.h
+ * Namespace:       none
+ * Description:     Header file which includes all
+ *                  z::file methods.
+ *
+ *
+ * Author:          Zachary Westerman
+ * Email:           zacharywesterman@yahoo.com
+ * Last modified:   29 Mar. 2017
+**/
+
+#pragma once
+#ifndef FILE_H_INCLUDED
+#define FILE_H_INCLUDED
+
+#include "file/list_files_in_dir.h"
+#include "file/directory_operations.h"
+
+#include "file/loadFileToMemory.h"
+#include "file/saveToFileFromMemory.h"
+
+#endif // FILE_H_INCLUDED
