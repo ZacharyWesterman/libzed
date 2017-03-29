ec58a0a1294322eed631aa1d47c89231521dfc81
Zachary Westerman <zacharywesterman@yahoo.com>
2017-03-29 17:17:33 -0500

    added z::core and z::math headers

---
+++ z/core.h
@@ -0,0 +1,38 @@
+/**
+ * File:            core.h
+ * Namespace:       none
+ * Description:     Header file which includes all
+ *                  z::core functions and datatypes.
+ *
+ *
+ * Author:          Zachary Westerman
+ * Email:           zacharywesterman@yahoo.com
+ * Last modified:   1 Mar. 2017
+**/
+
+
+#pragma once
+#ifndef CORE_H_INCLUDED
+#define CORE_H_INCLUDED
+
+///Strings
+#include "core/string.h"
+#include "core/stream.h"
+
+///Arrays
+#include "core/array.h"
+#include "core/sorted_array.h"
+#include "core/sorted_ref_array.h"
+
+///Misc data structures
+#include "core/dynamicStack.h"
+#include "core/circular_number.h"
+#include "core/timer.h"
+
+///Misc functions
+#include "core/pause.h"
+
+///Dynamic library loading
+#include "core/loadLibrary.h"
+
+#endif // CORE_H_INCLUDED
---
+++ z/math.h
@@ -0,0 +1,26 @@
+/**
+ * File:            math.h
+ * Namespace:       none
+ * Description:     Header file which includes all
+ *                  z::math functions and constants.
+ *
+ *
+ * Author:          Zachary Westerman
+ * Email:           zacharywesterman@yahoo.com
+ * Last modified:   3 Mar. 2017
+**/
+
+
+#pragma once
+#ifndef MATH_H_INCLUDED
+#define MATH_H_INCLUDED
+
+///Constants
+#include "math/constants.h"
+
+///Functions
+#include "math/factorial.h"
+#include "math/fibonacci.h"
+
+
+#endif // MATH_H_INCLUDED
