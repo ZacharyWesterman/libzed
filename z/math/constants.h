0f32e5449c725b79b187ae28051ea16add88f99f
Zachary Westerman <zacharywesterman@yahoo.com>
2017-03-29 17:15:23 -0500

    math constants and fast Fibonacci sequence

---
+++ z/math/constants.h
@@ -0,0 +1,43 @@
+/**
+ * File:            constants.h
+ * Namespace:       z::math
+ *
+ * Description:     Some global mathematical constants.
+ *                  IEEE doubles only have ~15 decimal
+ *                  place precision, but to avoid
+ *                  rounding errors I have made these
+ *                  constants as accurate as I
+ *                  reasonably can.
+ *
+ *
+ * Author:          Zachary Westerman
+ * Email:           zacharywesterman@yahoo.com
+ * Last modified:   3 Mar. 2017
+**/
+
+#pragma once
+#ifndef CONSTANTS_H_INCLUDED
+#define CONSTANTS_H_INCLUDED
+
+namespace z
+{
+    namespace math
+    {
+        //pi to the 31st decimal place.
+        const double pi     = 3.1415926535897932384626433832795;
+
+        //e to the 31st decimal place.
+        const double e      = 2.7182818284590452353602874713527;
+
+        //Pythagoras' constant to the 65th decimal place.
+        const double sqrt2  = 1.41421356237309504880168872420969807856967187537694807317667973799;
+
+        //The square root of 5 to the 59th decimal place.
+        const double sqrt5  = 2.23606797749978969640917366873127623544061835961152572427089;
+
+        //The golden ratio to the 55th decimal place.
+        const double phi    = 1.6180339887498948482045868343656381177203091798057628621;
+    }
+}
+
+#endif // CONSTANTS_H_INCLUDED
---
+++ z/math/fibonacci.h
@@ -0,0 +1,54 @@
+/**
+ * File:            fibonacci.h
+ * Namespace:       z::math
+ *
+ * Description:     The fibonacci(n) function returns
+ *                  the nth number in the Fibonacci
+ *                  sequence. To avoid recursion and
+ *                  for the sake of performance, this
+ *                  function uses Binet's formula.
+ *
+ *                  Additionally, doubles are used to
+ *                  allow for very large output values,
+ *                  but anything up to fibonacci(46)
+ *                  number safely be cast to a (long), up to
+ *                  the fibonacci(92) can be cast to
+ *                  (unsigned long) or a (long long). However,
+ *                  the result is only accurate up to
+ *                  fibonacci(71).
+ *
+ *                  Note: this function will return
+ *                  nan (not a number) if the parameter
+ *                  is anything other than a whole number.
+ *                  It does, however, work with negatives.
+ *
+ *
+ * Author:          Zachary Westerman
+ * Email:           zacharywesterman@yahoo.com
+ * Last modified:   3 Feb. 2017
+**/
+
+#pragma once
+#ifndef FIBONACCI_H_INCLUDED
+#define FIBONACCI_H_INCLUDED
+
+#include "constants.h"
+#include <math.h>
+
+namespace z
+{
+    namespace math
+    {
+        double fibonacci(double n)
+        {
+            double PHI_n  = pow(phi, n);
+            double Nphi_n = pow(-phi + 1, n);
+
+            double fib_n  = (PHI_n - Nphi_n) / sqrt5;
+
+            return fib_n;
+        }
+    }
+}
+
+#endif // FIBONACCI_H_INCLUDED
