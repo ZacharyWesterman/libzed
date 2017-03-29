0b832d558b28aabe197e395a5a1beb84f5e8ede8
Zachary Westerman <zacharywesterman@yahoo.com>
2017-03-29 17:14:46 -0500

    added dynamic library loading methods

---
+++ z/core/loadLibrary.h
@@ -0,0 +1,62 @@
+/**
+ * File:            lodaLibrary.h
+ * Namespace:       z::core::library
+ *
+ * Description:     A platform-independent set of
+ *                  functions for loading and unloading
+ *                  libraries, and getting the functions
+ *                  in those libraries.
+ *
+ *
+ * Author:          Zachary Westerman
+ * Email:           zacharywesterman@yahoo.com
+ * Last modified:   3 Feb. 2017
+**/
+
+#pragma once
+#ifndef LOADLIBRARY_H_INCLUDED
+#define LOADLIBRARY_H_INCLUDED
+
+#include "string.h"
+#include <stdint.h>
+
+#ifdef __linux__
+    #error "loadLibrary.h" is incompatible with Linux.
+#elif _WIN32
+
+#include <windows.h>
+
+namespace z
+{
+    namespace core
+    {
+        namespace library
+        {
+            typedef HINSTANCE lib_t;
+
+            typedef FARPROC proc_t;
+
+
+            lib_t load(const string<char>& file_name)
+            {
+                return LoadLibrary(file_name.str());
+            }
+
+            proc_t get_function(lib_t Library, const string<char>& func_name)
+            {
+                return GetProcAddress(Library, func_name.str());
+            }
+
+            bool unload(lib_t Library)
+            {
+                return (bool)FreeLibrary(Library);
+            }
+        }
+    }
+}
+
+#else
+    #error "loadLibrary.h" is incompatible with target OS.
+#endif
+
+#endif // LOADLIBRARY_H_INCLUDED
