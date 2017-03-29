727f38a3e7b9a2f021448de44c0e8dbf9a6ef881
Zachary Westerman <zacharywesterman@yahoo.com>
2017-03-29 18:15:05 -0500

    square matrix test

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include <z/core.h>
 #include <z/math.h>
 
@@ -7,11 +8,16 @@ using namespace z;
 
 int main()
 {
-    core::string<wchar_t> wide = "Something";
+    core::string<wchar_t> wide;
 
-    core::string<char> narr = wide;
+    math::matrix<3> mat;
 
-    cout << narr.str() << endl;
+    mat.value[0][0] = 1;
+    mat.value[0][1] = 0;
+    mat.value[1][0] = 0;
+    mat.value[1][1] = 2;
+
+    cout << mat.det() << endl;
 
     return 0;
 }
