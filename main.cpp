1af3c8e58d66b59e617c1a0271a7a44f263e33e9
Unknown <zacharywesterman@yahoo.com>
2017-04-16 21:03:19 -0500

    saved layout

--- main.cpp
+++ main.cpp
@@ -9,43 +9,16 @@ using namespace z;
 
 int main()
 {
-    core::string<char> String = "ten=";
+    core::string<wchar_t> wide;
 
-    String+=10;
-    cout << String.str() << endl;
+    math::matrix<2> mat;
 
+    mat.value[0][0] = 1;
+    mat.value[0][1] = 0;
+    mat.value[1][0] = 0;
+    mat.value[1][1] = 2;
 
-    cout << "\nDone.\n";
-
-    /*cout << endl;
-    cout << "( " << (int)'(' << ':' << (int)L'(' << endl;
-    cout << ") " << (int)')' << ':' << (int)L')' << endl;
-    cout << "[ " << (int)'[' << ':' << (int)L'[' << endl;
-    cout << "] " << (int)']' << ':' << (int)L']' << endl;
-    cout << "{ " << (int)'{' << ':' << (int)L'{' << endl;
-    cout << "} " << (int)'}' << ':' << (int)L'}' << endl;
-    cout << "\" " << (int)'"' << ':' << (int)L'"' << endl;
-    cout << "  " << (int)' ' << ':' << (int)L' ' << endl;
-    cout << ". " << (int)'.' << ':' << (int)L'.' << endl;
-    cout << ", " << (int)',' << ':' << (int)L',' << endl;
-    cout << "_ " << (int)'_' << ':' << (int)L'_' << endl;
-    cout << ": " << (int)':' << ':' << (int)L':' << endl;
-    cout << "; " << (int)';' << ':' << (int)L';' << endl;
-    cout << "\\n"<< (int)'\n'<< ':' << (int)L'\n'<< endl;
-    cout << "- "<< (int)'-'<< ':' << (int)L'-'<< endl;
-    cout << "+ "<< (int)'+'<< ':' << (int)L'+'<< endl;
-    cout << "0 "<< (int)'0'<< ':' << (int)L'0'<< endl;
-    cout << "1 "<< (int)'1'<< ':' << (int)L'1'<< endl;
-    cout << "7 "<< (int)'7'<< ':' << (int)L'7'<< endl;
-    cout << "9 "<< (int)'9'<< ':' << (int)L'9'<< endl;
-    cout << "a "<< (int)'a'<< ':' << (int)L'a'<< endl;
-    cout << "f "<< (int)'f'<< ':' << (int)L'f'<< endl;
-    cout << "z "<< (int)'z'<< ':' << (int)L'z'<< endl;
-    cout << "A "<< (int)'A'<< ':' << (int)L'A'<< endl;
-    cout << "F "<< (int)'F'<< ':' << (int)L'F'<< endl;
-    cout << "Z "<< (int)'Z'<< ':' << (int)L'Z'<< endl;
-    cout << "= "<< (int)'='<< ':' << (int)L'='<< endl;
-    cout << "\\ "<< (int)'\\'<< ':' << (int)L'\\'<< endl;*/
+    cout << mat.det() << endl;
 
     return 0;
 }
