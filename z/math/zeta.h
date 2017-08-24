90f25ce634a763cceca5945d7dcfc924a375c2f3
Unknown <zacharywesterman@yahoo.com>
2017-08-24 16:11:10 -0500

    updated zeta function to be more accurate

--- z/math/zeta.h
+++ z/math/zeta.h
@@ -3,12 +3,14 @@
  * Namespace:       z::math
  *
  * Description:     Constexpr function for approximating the
- *                  reimann-zeta sum.
+ *                  reimann-zeta sum. Note that the
+ *                  approximation is less accurate for
+ *                  single-precision floats.
  *
  *
  * Author:          Zachary Westerman
  * Email:           zacharywesterman@yahoo.com
- * Last modified:   21 Aug. 2017
+ * Last modified:   24 Aug. 2017
 **/
 
 #pragma once
@@ -20,24 +22,30 @@ namespace z
     namespace math
     {
         //compute zeta(s) with highest possible precision
+        ///for zeta(s), it is assumed that s>1
         zFloat zeta(const int s)
         {
             zFloat result = 1.0;
+            zFloat oldResult = 0.0;
 
             int i = 2;
-            zFloat z_denom = 0.5;
 
-            while ( (z_denom + result) > result )
+            while (result != oldResult)
             {
-                result += z_denom;
+                oldResult = result;
+
+                zFloat denomP = 1.0 / (zFloat)i;
+
+                zFloat partial = 1;
+                for (int j=0; j<s; j++)
+                    partial *= denomP;
+
+                result += partial;
 
                 i++;
-                int power = 1;
-                for (int j=0; j<s; j++)
-                    power *= i;
-
-                z_denom = 1.0 / (zFloat)power;
             }
+
+            //std::cout << i << std::endl;
 
             return result;
         }
