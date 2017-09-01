ce36110afe27440ccceccf7311a09186b0ee7d9e
Unknown <zacharywesterman@yahoo.com>
2017-08-31 21:16:43 -0500

    updated documentation

--- Doxygen/html/search/all_a.js
+++ Doxygen/html/search/all_a.js
@@ -4,6 +4,5 @@ var searchData=
   ['remove',['remove',['../classz_1_1core_1_1array.html#a334e01bca2d971f23b27eefb8bed0129',1,'z::core::array']]],
   ['remove_5fwhitespace',['remove_whitespace',['../namespacez_1_1core.html#a2b55daae52ae9276da5490a35d148ba0',1,'z::core']]],
   ['remove_5fwhitespace_5fl',['remove_whitespace_L',['../namespacez_1_1core.html#a2365d3a9589a81011b906ef3f237e629',1,'z::core']]],
-  ['remove_5fwhitespace_5fr',['remove_whitespace_R',['../namespacez_1_1core.html#abd1123c1be0a77ec364f40695d76bf2a',1,'z::core']]],
-  ['replace',['replace',['../classz_1_1core_1_1string.html#ac54ae32998e82f931d68c1649e7264dd',1,'z::core::string']]]
+  ['remove_5fwhitespace_5fr',['remove_whitespace_R',['../namespacez_1_1core.html#abd1123c1be0a77ec364f40695d76bf2a',1,'z::core']]]
 ];
--- Doxygen/html/search/functions_3.js
+++ Doxygen/html/search/functions_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['find',['find',['../classz_1_1core_1_1array.html#a40863d5a8c5134a849e13f84a11889e6',1,'z::core::array::find()'],['../classz_1_1core_1_1sortedArray.html#ad1b10e1ac2d25f8e73c278bce5a39189',1,'z::core::sortedArray::find()'],['../classz_1_1core_1_1string.html#a2f92c34c272a1bc90d13af3ce488fb49',1,'z::core::string::find()']]]
+  ['find',['find',['../classz_1_1core_1_1array.html#a40863d5a8c5134a849e13f84a11889e6',1,'z::core::array::find()'],['../classz_1_1core_1_1sortedArray.html#ad1b10e1ac2d25f8e73c278bce5a39189',1,'z::core::sortedArray::find()']]]
 ];
