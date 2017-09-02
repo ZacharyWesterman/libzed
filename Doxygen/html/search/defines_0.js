820d4d55c0805be05076621c15d8380424873a46
Unknown <zacharywesterman@yahoo.com>
2017-09-01 20:47:11 -0500

    saved layout

--- Doxygen/html/search/defines_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['zeta_5fh_5fincluded',['ZETA_H_INCLUDED',['../zeta_8h.html#a58428223dbd8314284fd528ac4588d67',1,'zeta.h']]]
-];
---
+++ Doxygen/html/search/functions_b.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['zeta',['zeta',['../a00107.html#a05b6bcbb4297f9aed089308619c91f2a',1,'z::math']]]
+];
