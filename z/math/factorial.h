ed736f474e6994db08cdda9ce36093cbbacd569a
Zachary <zacharywesterman@yahoo.com>
2018-01-12 21:16:57 -0600

    updated documentation for math::factorial()

--- z/math/factorial.h
+++ z/math/factorial.h
@@ -12,6 +12,7 @@ namespace z
     {
         /**
          * \brief Calculate the factorial of the integer \b n.
+         * Does not check for overflow.
          *
          * \param n a positive integer.
          *
@@ -44,6 +45,16 @@ namespace z
             return result;
         }
 
+        /**
+         * \brief Calculate the factorial of the integer \b n, but let
+         * us know if it overflowed.
+         *
+         * \param n a positive integer.
+         * \param did_overflow whether the calculation overflowed, returned by reference.
+         *
+         * \return The factorial of \b n, if <B>n>0</B> and
+         * the factorial is able to be represented. \b 0 otherwise.
+         */
         template<typename INT,
             typename = typename std::enable_if<std::is_integral<INT>::value>::type>
         INT factorial(INT n, bool& did_overflow)
