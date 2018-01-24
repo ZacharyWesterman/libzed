babf9811c4492df4d61ac7c627dc0cc923a455b1
Unknown <zacharywesterman@yahoo.com>
2018-01-23 21:01:04 -0600

    added header to include all primitive definitions

---
+++ z/z.h
@@ -0,0 +1,9 @@
+/**
+ * \file z.h
+ * \namespace z
+ * \brief Header that includes defines and structs required by all other libraries.
+**/
+
+#include "char.h"
+#include "int.h"
+#include "float.h"
