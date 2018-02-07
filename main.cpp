d4c2b84c2dc4f3003bf6daba756618d9e94cdcf4
Unknown <zacharywesterman@yahoo.com>
2018-02-06 22:48:28 -0600

    saved layout

--- main.cpp
+++ main.cpp
@@ -1,45 +1,18 @@
-#include <iostream>
+//#include <iostream>
 
 //#define Z_USE_DOUBLE
 
 #include <z/core.h>
-#include <z/util.h>
+#include <z/file.h>
 
-using namespace std;
 using namespace z;
 
-class output : public core::stream<char>
-{
-public:
-    virtual void putChar(char c) {cout << c;}
-
-    virtual char getChar() {return 0;}
-
-    virtual output& operator<<(const core::string<char>& input)
-    {
-        cout << input.str();
-
-        return *this;
-    }
-
-    virtual output& operator>>(core::string<char>& output) {return *this;}
-
-    virtual bool eof()
-    {
-        return 0;
-    }
-};
-
-void print(core::stream<char>& stream)
-{
-    stream.putChar('a');
-}
 
 int main()
 {
-    output out;
+    core::console console;
 
-    print (out);
+    console.write(file::workingDir());
 
     return 0;
 }
