bcf2523a50cbbe7e6a064878de8f0bab4031dbeb
Unknown <zacharywesterman@yahoo.com>
2018-02-06 22:45:57 -0600

    rafactored core::stream class

--- z/core/stream.h
+++ z/core/stream.h
@@ -1,50 +1,48 @@
 #pragma once
-#ifndef STREAM_H_INCLUDED
-#define STREAM_H_INCLUDED
-
-#include <type_traits>
+#ifndef CORE_STREAM_H_INCLUDED
+#define CORE_STREAM_H_INCLUDED
 
 #include "string.h"
-
-#ifndef null
-    #define null 0
-#endif // null
-
-static const int ETX = 3; //end of text
-static const int SI  = 15; //shift-in
-static const int SO  = 14; //shift-out
 
 namespace z
 {
     namespace core
     {
+        template <typename CHAR>
+        class inputStream
+        {
+        public:
+            virtual ~inputStream() {}
+
+            virtual CHAR get() = 0;
+            virtual core::string<CHAR> read(CHAR delim = ' ') = 0;
+
+            virtual bool empty() = 0;
+        };
+
+        template <typename CHAR>
+        class outputStream
+        {
+        public:
+            virtual ~outputStream() {}
+
+            virtual void put(CHAR) = 0;
+            virtual void write(const core::string<CHAR>&) = 0;
+
+            virtual bool empty() = 0;
+        };
+
         /**
          * \brief A template class for character streams.
          *
          *
          */
         template <typename CHAR>
-        class stream
+        class stream : public inputStream<CHAR>, public outputStream<CHAR>
         {
-        protected:
-
-
         public:
             virtual ~stream() {}
-
-            virtual void putChar(CHAR) = 0;
-
-            virtual CHAR getChar() = 0;
-
-            ///Operator for input to stream
-            virtual stream& operator<<(string<CHAR>&) = 0;
-
-            ///Operator for output from stream
-            virtual stream& operator>>(string<CHAR>&) = 0;
-
-            virtual bool eof() = 0;
         };
-
     }
 }
 
