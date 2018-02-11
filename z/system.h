521f61574eb3ce2a3b78872bfdc45f5500c85ff1
Unknown <zacharywesterman@yahoo.com>
2018-02-11 10:59:16 -0600

    added z::system namespace

---
+++ z/system.h
@@ -0,0 +1,16 @@
+/**
+ * \file z/system.h
+ * \namespace z::system
+ * \brief Library containing classes and functions relating to system info.
+ *
+**/
+
+#pragma once
+#ifndef SYSTEM_H_INCLUDED
+#define SYSTEM_H_INCLUDED
+
+#include "system/console.h"
+
+#include "system/cpuid.h"
+
+#endif // SYSTEM_H_INCLUDED
