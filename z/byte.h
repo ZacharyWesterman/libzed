df698e404733360144d674ff696bd0218327c7fd
Zachary Westerman <westerman.zachary@gmail.com>
2018-06-21 20:33:31 -0500

    added z::byte type

---
+++ z/byte.h
@@ -0,0 +1,19 @@
+/**
+ * \file z/byte.h
+ *
+ * Contains default single-byte type.
+**/
+
+#pragma once
+#ifndef BYTE_H_INCLUDED
+#define BYTE_H_INCLUDED
+
+namespace z
+{
+	/**
+	 * \brief Default byte type used by other libraries.
+	 */
+	typedef char byte;
+}
+
+#endif //BYTE_H_INCLUDED
--- z/z.h
+++ z/z.h
@@ -4,6 +4,7 @@
  * \brief Header that includes defines and structs required by all other libraries.
 **/
 
+#include "byte.h"
 #include "char.h"
 #include "int.h"
 #include "float.h"
