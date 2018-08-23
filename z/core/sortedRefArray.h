502c67ae27f0e6a1fc38a54e2d8d7c0558cf08e3
Zachary Westerman <westerman.zachary@gmail.com>
2018-08-22 21:18:04 -0500

    Separating implementations

--- z/core/sortedRefArray.h
+++ z/core/sortedRefArray.h
@@ -6,267 +6,267 @@
 
 namespace z
 {
-    namespace core
-    {
-        /**
-         * \brief An extension of the core::refArray class which
-         * attempts to keep all data sorted.
-         *
-         * Unlike with the <B>sorted array</B>, elements are
-         * assumed to be pointers and are sorted
-         * with this in mind.
-         *
-         * \see array
-         * \see refArray
-         * \see sortedArray
-         */
-        template <typename T>
-        class sortedRefArray : public refArray<T>
-        {
-        private:
-            inline void init(const T arg1)
-            {
-                add(arg1);
-            }
+	namespace core
+	{
+		/**
+		 * \brief An extension of the core::refArray class which
+		 * attempts to keep all data sorted.
+		 *
+		 * Unlike with the <B>sorted array</B>, elements are
+		 * assumed to be pointers and are sorted
+		 * with this in mind.
+		 *
+		 * \see array
+		 * \see refArray
+		 * \see sortedArray
+		 */
+		template <typename T>
+		class sortedRefArray : public refArray<T>
+		{
+		private:
+			inline void init(const T arg1)
+			{
+				add(arg1);
+			}
 
-            template <typename... Args>
-            inline void init(const T arg1, const Args... args)
-            {
-                add(arg1);
+			template <typename... Args>
+			inline void init(const T arg1, const Args... args)
+			{
+				add(arg1);
 
-                init(args...);
-            }
+				init(args...);
+			}
 
-        public:
-            ///Default constructor
-            sortedRefArray() {}
+		public:
+			///Default constructor
+			sortedRefArray() {}
 
-            sortedRefArray(const refArray<T>&);
-            sortedRefArray(const T);
+			sortedRefArray(const refArray<T>&);
+			sortedRefArray(const T);
 
-            template <typename... Args>
-            sortedRefArray(const T arg1, const Args... args);
+			template <typename... Args>
+			sortedRefArray(const T arg1, const Args... args);
 
-            virtual int add(const T&);
-            virtual void add(const refArray<T>&);
+			virtual int add(const T&);
+			virtual void add(const refArray<T>&);
 
-            int find(const T&) const;
-            int findInsert(const T) const;
+			int find(const T&) const;
+			int findInsert(const T) const;
 
-            virtual void sort();
-        };
+			virtual void sort();
+		};
 
-        ///Copy constructor
-        template <typename T>
-        sortedRefArray<T>::sortedRefArray(const refArray<T>& other)
-        {
-            for (int i=0; i<other.size(); i++)
-                add(other[i]);
-        }
+		///Copy constructor
+		template <typename T>
+		sortedRefArray<T>::sortedRefArray(const refArray<T>& other)
+		{
+			for (int i=0; i<other.size(); i++)
+				add(other[i]);
+		}
 
-        /**
-         * \brief Constructor with one argument.
-         *
-         * Constructs the array with one
-         * element already contained.<BR>
-         *
-         * \b Syntax: array<T> X (arg1); array<T> X = arg1;
-         *
-         * \param arg1 initializing data.
-         */
-        template <typename T>
-        sortedRefArray<T>::sortedRefArray(const T arg1)
-        {
-            this->array_data.push_back(arg1);
-        }
-
-
-        /**
-         * \brief List-initialized constructor.
-         *
-         * Constructs the array with an arbitrary
-         * number of elements already contained and
-         * sorted.<BR>
-         *
-         * \b Syntax: array<T> X {arg1, arg2, ...};
-         * array<T> X = {arg1, arg2, ...};
-         *
-         * \param arg1 initializing data.
-         * \param args cont. initializing data.
-         */
-        template <typename T>
-        template <typename... Args>
-        sortedRefArray<T>::sortedRefArray(const T arg1, const Args... args)
-        {
-            init(arg1, args...);
-        }
+		/**
+		 * \brief Constructor with one argument.
+		 *
+		 * Constructs the array with one
+		 * element already contained.<BR>
+		 *
+		 * \b Syntax: array<T> X (arg1); array<T> X = arg1;
+		 *
+		 * \param arg1 initializing data.
+		 */
+		template <typename T>
+		sortedRefArray<T>::sortedRefArray(const T arg1)
+		{
+			this->array_data.push_back(arg1);
+		}
 
 
-        /**
-         * \brief Add an object pointer to the array.
-         *
-         * Finds an appropriate location and inserts the
-         * pointer into the array, such that the array
-         * remains sorted.
-         *
-         * \param object the pointer to add to the array.
-         *
-         * \return The index where the inserted pointer now resides.
-         */
-        template <typename T>
-        int sortedRefArray<T>::add(const T& object)
-        {
-            int index = findInsert(object);
-
-            this->insert(object, index);
-
-            return index;
-        }
-
-        /**
-         * \brief Add the contents of another array to this one.
-         *
-         * For each of the elements in the other array,
-         * this function finds an appropriate location in this
-         * array and inserts the element, such that this array
-         * remains sorted.
-         *
-         * \param other the array to copy from.
-         */
-        template <typename T>
-        void sortedRefArray<T>::add(const refArray<T>& other)
-        {
-            for (int i=0; i<other.size(); i++)
-            {
-                int index = findInsert(other[i]);
-
-                this->insert(other[i], index);
-            }
-        }
-
-        /**
-         * \brief Check if a given object with identical
-         * data is in the array.
-         *
-         * Locates the desired index using a binary search,
-         * as the array is expected to be sorted.
-         *
-         * \param object a pointer to the object to search for.
-         *
-         * \return The first index that the object data was found at.
-         * \b -1 if it was not found.
-         */
-        template <typename T>
-        int sortedRefArray<T>::find(const T& object) const
-        {
-            if (this->array_data.size() == 0)
-                return -1;
-
-            int left = 0;
-            int right = this->array_data.size()-1;
-
-            while (left < right)
-            {
-                int center = (left + right) / 2;
-
-                if (*(this->array_data.at(center)) < *object)
-                {
-                    left = center + 1;
-                }
-                else if (*(this->array_data.at(center)) > *object)
-                {
-                    right = center - 1;
-                }
-                else
-                {
-                    return center;
-                }
-            }
-
-            if (*(this->array_data.at(left)) == *object)
-                return left;
-            else
-                return -1;
-        }
-
-        /**
-         * \brief Find an index where the given object pointer
-         * can be inserted while keeping the array sorted.
-         *
-         * Locates the desired index using a binary search,
-         * as the array is expected to be sorted.
-         *
-         * \param object pointer to the object to search for.
-         *
-         * \return The first index where the pointer can be
-         * inserted.
-         */
-        template <typename T>
-        int sortedRefArray<T>::findInsert(const T object) const
-        {
-            if (this->array_data.size() == 0)
-                return 0;
-
-            int left = 0;
-            int right = this->array_data.size()-1;
-
-            while (left < right)
-            {
-                int center = (left + right) / 2;
-
-                if (*(this->array_data.at(center)) < *object)
-                {
-                    left = center + 1;
-                }
-                else if (*(this->array_data.at(center)) > *object)
-                {
-                    right = center - 1;
-                }
-                else
-                {
-                    return center;
-                }
-            }
-
-            if (*(this->array_data.at(left)) < *object)
-                return left+1;
-            else
-                return left;
-        }
+		/**
+		 * \brief List-initialized constructor.
+		 *
+		 * Constructs the array with an arbitrary
+		 * number of elements already contained and
+		 * sorted.<BR>
+		 *
+		 * \b Syntax: array<T> X {arg1, arg2, ...};
+		 * array<T> X = {arg1, arg2, ...};
+		 *
+		 * \param arg1 initializing data.
+		 * \param args cont. initializing data.
+		 */
+		template <typename T>
+		template <typename... Args>
+		sortedRefArray<T>::sortedRefArray(const T arg1, const Args... args)
+		{
+			init(arg1, args...);
+		}
 
 
-        /**
-         * \brief Sorts the array.
-         *
-         * This function currently uses bubble-sort. As
-         * such, it is <B>very slow</B> for large arrays!
-         * I intend to implement a faster version later.
-         */
-        template <typename T>
-        void sortedRefArray<T>::sort()
-        {
-            bool done = true;
+		/**
+		 * \brief Add an object pointer to the array.
+		 *
+		 * Finds an appropriate location and inserts the
+		 * pointer into the array, such that the array
+		 * remains sorted.
+		 *
+		 * \param object the pointer to add to the array.
+		 *
+		 * \return The index where the inserted pointer now resides.
+		 */
+		template <typename T>
+		int sortedRefArray<T>::add(const T& object)
+		{
+			int index = findInsert(object);
 
-            do
-            {
-                done = true;
+			this->insert(object, index);
+
+			return index;
+		}
+
+		/**
+		 * \brief Add the contents of another array to this one.
+		 *
+		 * For each of the elements in the other array,
+		 * this function finds an appropriate location in this
+		 * array and inserts the element, such that this array
+		 * remains sorted.
+		 *
+		 * \param other the array to copy from.
+		 */
+		template <typename T>
+		void sortedRefArray<T>::add(const refArray<T>& other)
+		{
+			for (int i=0; i<other.size(); i++)
+			{
+				int index = findInsert(other[i]);
+
+				this->insert(other[i], index);
+			}
+		}
+
+		/**
+		 * \brief Check if a given object with identical
+		 * data is in the array.
+		 *
+		 * Locates the desired index using a binary search,
+		 * as the array is expected to be sorted.
+		 *
+		 * \param object a pointer to the object to search for.
+		 *
+		 * \return The first index that the object data was found at.
+		 * \b -1 if it was not found.
+		 */
+		template <typename T>
+		int sortedRefArray<T>::find(const T& object) const
+		{
+			if (this->array_data.size() == 0)
+				return -1;
+
+			int left = 0;
+			int right = this->array_data.size()-1;
+
+			while (left < right)
+			{
+				int center = (left + right) / 2;
+
+				if (*(this->array_data.at(center)) < *object)
+				{
+					left = center + 1;
+				}
+				else if (*(this->array_data.at(center)) > *object)
+				{
+					right = center - 1;
+				}
+				else
+				{
+					return center;
+				}
+			}
+
+			if (*(this->array_data.at(left)) == *object)
+				return left;
+			else
+				return -1;
+		}
+
+		/**
+		 * \brief Find an index where the given object pointer
+		 * can be inserted while keeping the array sorted.
+		 *
+		 * Locates the desired index using a binary search,
+		 * as the array is expected to be sorted.
+		 *
+		 * \param object pointer to the object to search for.
+		 *
+		 * \return The first index where the pointer can be
+		 * inserted.
+		 */
+		template <typename T>
+		int sortedRefArray<T>::findInsert(const T object) const
+		{
+			if (this->array_data.size() == 0)
+				return 0;
+
+			int left = 0;
+			int right = this->array_data.size()-1;
+
+			while (left < right)
+			{
+				int center = (left + right) / 2;
+
+				if (*(this->array_data.at(center)) < *object)
+				{
+					left = center + 1;
+				}
+				else if (*(this->array_data.at(center)) > *object)
+				{
+					right = center - 1;
+				}
+				else
+				{
+					return center;
+				}
+			}
+
+			if (*(this->array_data.at(left)) < *object)
+				return left+1;
+			else
+				return left;
+		}
 
 
-                for (int i=0; i<(int)this->array_data.size()-1; i++)
-                {
-                    if (*(this->array_data[i]) > *(this->array_data[i+1]))
-                    {
-                        done = false;
+		/**
+		 * \brief Sorts the array.
+		 *
+		 * This function currently uses bubble-sort. As
+		 * such, it is <B>very slow</B> for large arrays!
+		 * I intend to implement a faster version later.
+		 */
+		template <typename T>
+		void sortedRefArray<T>::sort()
+		{
+			bool done = true;
 
-                        T temp = this->array_data[i];
-                        this->array_data[i] = this->array_data[i+1];
-                        this->array_data[i+1] = temp;
-                    }
-                }
-            }
-            while (!done);
-        }
-    }
+			do
+			{
+				done = true;
+
+
+				for (int i=0; i<(int)this->array_data.size()-1; i++)
+				{
+					if (*(this->array_data[i]) > *(this->array_data[i+1]))
+					{
+						done = false;
+
+						T temp = this->array_data[i];
+						this->array_data[i] = this->array_data[i+1];
+						this->array_data[i+1] = temp;
+					}
+				}
+			}
+			while (!done);
+		}
+	}
 }
 
 #endif // SORTEDREFARRAY_H_INCLUDED
---
+++ z/core/timer.cpp
@@ -0,0 +1,43 @@
+#include "timer.h"
+
+namespace z
+{
+	namespace core
+	{
+		timer::timer (bool run)
+		{
+			if (run) reset();
+		}
+
+		void timer::reset()
+		{
+			start = std::chrono::high_resolution_clock::now();
+		}
+
+		size_t timer::micros() const
+		{
+			return (size_t)std::chrono::duration_cast<std::chrono::microseconds>
+					(std::chrono::high_resolution_clock::now() - start).count();
+		}
+
+		size_t timer::millis() const
+		{
+			return (micros() / 1000);
+		}
+
+		size_t timer::seconds() const
+		{
+			return (micros() / 1000000);
+		}
+
+		size_t timer::minutes() const
+		{
+			return (micros() / 60000000);
+		}
+
+		size_t timer::hours() const
+		{
+			return (micros() / 3600000000);
+		}
+	}
+}
