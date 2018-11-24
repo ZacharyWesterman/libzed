e8ea88632f7478683b4adfdaae46cdff7c60eece
ZacharyWesterman <westerman.zachary@gmail.com>
2018-11-24 12:28:43 -0600

    Added file::dir method to get the current working directory

---
+++ z/file/dir.cpp
@@ -0,0 +1,28 @@
+#include "dir.h"
+
+#include <stdio.h>
+#ifdef _WIN32
+#include <direct.h>
+#elif __linux__
+#include <unistd.h>
+#define _getcwd getcwd
+#else
+#warning file::path is incompatible with target OS!
+#endif
+
+namespace z
+{
+	namespace file
+	{
+		core::string<utf8> dir()
+		{
+#			if defined(_WIN32) || defined(__linux__)
+			char buf[FILENAME_MAX];
+			_getcwd(buf, FILENAME_MAX);
+			return buf;
+#			else
+			return ".";
+#			endif
+		}
+	}
+}
---
+++ z/file/dir.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <z/core/string.h>
+
+namespace z
+{
+	namespace file
+	{
+		/**
+		 * \brief Get the current working directory.
+		 * \return A string containing the current directory the executable is running from.
+		 * \threadsafe_function_yes
+		 */
+		core::string<utf8> dir();
+	}
+}
