d14989706c549a9b2ffef89d533ae89329da53e3
Zachary Westerman <westerman.zachary@gmail.com>
2019-01-27 20:12:51 -0600

    Documented core::split() and core::join()

--- z/core/join.h
+++ z/core/join.h
@@ -7,6 +7,16 @@ namespace z
 {
 	namespace core
 	{
+		/**
+		 * \brief Concatenate elements in an array into a string, separated by a delimiter.
+		 *
+		 * \param list An array of strings.
+		 * \param delim The delimiter to go between items.
+		 *
+		 * \return A string containing all elements in order, separated by the delimiter.
+		 *
+		 * \function_threadsafe_yes
+		 */
 		template <encoding E>
 		string<E> join(const array< string<E> >& list, const string<E>& delim)
 		{
--- z/core/split.h
+++ z/core/split.h
@@ -7,6 +7,17 @@ namespace z
 {
 	namespace core
 	{
+		/**
+		 * \brief Split a string into an array of strings according to a specified delimiter.
+		 *
+		 * \param input The string to split into an array.
+		 * \param delim The delimiter that separates items.
+		 *
+		 * \return A null array if the input string has 0 characters.
+		 * Otherwise, an array containing all substrings between any occurring delimiters.
+		 *
+		 * \function_threadsafe_yes
+		 */
 		template <encoding E>
 		array< string<E> > split(const string<E>& input, const string<E>& delim)
 		{
