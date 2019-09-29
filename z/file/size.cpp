1227da481897bc635c821bc068b74e89aafd5539
Zachary Westerman <westerman.zachary@gmail.com>
2019-09-29 13:02:14 -0500

    Added function to check file size

---
+++ z/file/size.cpp
@@ -0,0 +1,32 @@
+#include "size.h"
+
+#if __cplusplus == 201703L
+#include <filesystem>
+#else
+#include <fstream>
+#endif
+
+namespace z
+{
+	namespace file
+	{
+		size_t size(const zpath& path)
+		{
+#			if __cplusplus == 201703L
+			//If C++17, we can get file size faster than using fstream's seek() method.
+			std::error_code err{};
+			const auto len = std::filesystem::file_size((const char*)path.cstring());
+			return (err == std::error_code{}) ? 0 : len;
+#			else
+			//Otherwise default to fstream version.
+			std::ifstream in((const char*)path.cstring(), std::ios::binary);
+			if (in.bad()) return 0;
+			const auto begin = in.tellg();
+			in.seekg(0,std::ios::end);
+			const auto end = in.tellg();
+			in.close();
+			return end - begin;
+#			endif
+		}
+	}
+}
---
+++ z/file/size.h
@@ -0,0 +1,15 @@
+#include <z/core/string.h>
+
+namespace z
+{
+	namespace file
+	{
+		/**
+		 * \brief Get the size of a file in bytes
+		 *
+		 * \param path The file name.
+		 * \return The length of the file in bytes, or 0 if the file does not exist.
+		 */
+		size_t size(const zpath& path);
+	}
+}
