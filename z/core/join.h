e64d038cbdb6320a6f37023a499715ddb8c164d2
Zachary Westerman <westerman.zachary@gmail.com>
2020-01-11 17:03:07 -0600

    Let z::core::join() take an array of any object

--- z/core/join.h
+++ z/core/join.h
@@ -10,17 +10,41 @@ namespace z
 		/**
 		 * \brief Concatenate elements in an array into a string, separated by a delimiter.
 		 *
-		 * \param list An array of strings.
+		 * \param list An array whose elements can implicitly convert to strings.
 		 * \param delim The delimiter to go between items.
 		 *
 		 * \return A string containing all elements in order, separated by the delimiter.
 		 *
 		 * \threadsafe_function_yes
 		 */
-		template <encoding E>
-		string<E> join(const array< string<E> >& list, const string<E>& delim)
+		template <typename T, encoding E>
+		string<E> join(const array<T>& list, const string<E>& delim)
 		{
 			string<E> result;
+
+			for (auto& item : list)
+			{
+				if (result.length()) result += delim;
+				result += item;
+			}
+
+			return result;
+		}
+
+		/**
+		 * \brief Concatenate elements in an array into a string, separated by a delimiter.
+		 *
+		 * \param list An array whose elements can implicitly convert to strings.
+		 * \param delim The delimiter to go between items.
+		 *
+		 * \return A string containing all elements in order, separated by the delimiter.
+		 *
+		 * \threadsafe_function_yes
+		 */
+		template <typename T>
+		zstring join(const array<T>& list, const zstring& delim)
+		{
+			zstring result;
 
 			for (auto& item : list)
 			{
