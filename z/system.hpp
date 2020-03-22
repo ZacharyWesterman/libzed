dd71a6ba9b064f99d734f5d49b03826c5703752b
Zachary Westerman <westerman.zachary@gmail.com>
2020-03-22 14:34:09 -0500

    add stdin, stdout, stderr to system include

--- z/system.hpp
+++ z/system.hpp
@@ -6,6 +6,10 @@
  *
 **/
 
+#include "system/stdin.hpp"
+#include "system/stdout.hpp"
+#include "system/stderr.hpp"
+
 #include "system/console.hpp"
 #include "system/cpuid.hpp"
 #include "system/pause.hpp"
