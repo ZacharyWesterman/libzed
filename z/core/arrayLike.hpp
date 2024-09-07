51b9f4f34b145e91b014f3bb94a9c0c4974260fc
Zachary Westerman <westerman.zachary@gmail.com>
2024-09-06 22:38:28 -0500

    Abstract join() to allow any iterable, not just arrays

--- z/core/arrayLike.hpp
+++ z/core/arrayLike.hpp
@@ -12,7 +12,7 @@ namespace z
 		 * \brief An interface for all objects that can be both iterated over and directly indexed.
 		*/
 		template<typename T, typename ITER>
-		class arrayLike : public indexable<T>, iterable<ITER> {
+		class arrayLike : public indexable<T>, public iterable<ITER> {
 		public:
 			/**
 			* \brief Get the length of the array.
--- z/core/join.hpp
+++ z/core/join.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "string.hpp"
-#include "refArray.hpp"
+#include "iterable.hpp"
 
 namespace z
 {
@@ -10,7 +10,7 @@ namespace z
 		/**
 		* \brief Concatenate elements in an array into a string, separated by a delimiter.
 		*
-		* \param list An array whose elements can implicitly convert to strings.
+		* \param list An iterable whose elements can implicitly convert to strings.
 		* \param delim The delimiter to go between items.
 		*
 		* \return A string containing all elements in order, separated by the delimiter.
@@ -18,7 +18,7 @@ namespace z
 		* \threadsafe_function_yes
 		*/
 		template <typename T, encoding E>
-		string<E> join(const array<T>& list, const string<E>& delim) noexcept
+		string<E> join(const iterable<T>& list, const string<E>& delim) noexcept
 		{
 			string<E> result;
 
@@ -34,7 +34,7 @@ namespace z
 		/**
 		* \brief Concatenate elements in an array into a string, separated by a delimiter.
 		*
-		* \param list An array whose elements can implicitly convert to strings.
+		* \param list An iterable whose elements can implicitly convert to strings.
 		* \param delim The delimiter to go between items.
 		*
 		* \return A string containing all elements in order, separated by the delimiter.
@@ -42,7 +42,7 @@ namespace z
 		* \threadsafe_function_yes
 		*/
 		template <typename T>
-		zstring join(const array<T>& list, const zstring& delim) noexcept
+		zstring join(const iterable<T>& list, const zstring& delim) noexcept
 		{
 			zstring result;
 
@@ -58,7 +58,7 @@ namespace z
 		/**
 		* \brief Dereference elements in an array and concatenate them into a string, separated by a delimiter.
 		*
-		* \param list An array of pointers whose objects can implicitly convert to strings.
+		* \param list An iterable of pointers whose objects can implicitly convert to strings.
 		* \param delim The delimiter to go between items.
 		*
 		* \return A string containing all elements in order, separated by the delimiter.
@@ -66,7 +66,7 @@ namespace z
 		* \threadsafe_function_yes
 		*/
 		template <typename T, encoding E>
-		string<E> joinDeref(const array<T>& list, const string<E>& delim) noexcept
+		string<E> joinDeref(const iterable<T>& list, const string<E>& delim) noexcept
 		{
 			static_assert(std::is_pointer<T>::value, "Array template must be of pointer type.");
 
