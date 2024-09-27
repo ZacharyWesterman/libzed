dbb1a1f9c0fe9bc9f5594ba4f5df81c8b22d5536
Zachary Westerman <westerman.zachary@gmail.com>
2024-09-26 23:00:07 -0500

    break into multiple lines

--- z/core/zstr.hpp
+++ z/core/zstr.hpp
@@ -11,7 +11,12 @@ namespace zstr {
 /**
  * @brief Data types that a string can convert to or from.
  */
-enum zstr { string = 0, integer = 1, floating = 2, complex = 4 };
+enum zstr {
+	string = 0,
+	integer = 1,
+	floating = 2,
+	complex = 4,
+};
 } // namespace zstr
 } // namespace core
 } // namespace z
