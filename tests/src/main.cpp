59b47f88adf324f66657bd24370f6bbbe631847c
Zachary Westerman <westerman.zachary@gmail.com>
2024-09-29 02:04:01 -0500

    Add comments to tests/main.cpp

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,2 +1,6 @@
+// Do not add tests to this file.
+// This file is intentionally left mostly blank.
+// Catch2 will automatically generate the entry point.
+
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
