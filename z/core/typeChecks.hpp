e6b3de7835bf75ca35d8dd6e333c8a954cbbe0df
Zachary Westerman <westerman.zachary@gmail.com>
2026-06-13 19:21:59 -0500

    Add missing docs

--- z/core/typeChecks.hpp
+++ z/core/typeChecks.hpp
@@ -15,14 +15,19 @@ no operator>(const T &, const Arg &);
 template <typename T, typename Arg>
 no operator==(const T &, const Arg &);
 
+/// A template to enable specializations for an object if `operator<` exists on it.
 template <typename T, typename Arg = T>
 struct lesserExists {
 	enum { value = !std::is_same<decltype(std::declval<T>() < std::declval<Arg>()), no>::value };
 };
+
+/// A template to enable specializations for an object if `operator>` exists on it.
 template <typename T, typename Arg = T>
 struct greaterExists {
 	enum { value = !std::is_same<decltype(std::declval<T>() > std::declval<Arg>()), no>::value };
 };
+
+/// A template to enable specializations for an object if `operator==` exists on it.
 template <typename T, typename Arg = T>
 struct equalExists {
 	enum { value = !std::is_same<decltype(std::declval<T>() == std::declval<Arg>()), no>::value };
