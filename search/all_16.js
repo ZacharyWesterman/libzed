bf51ae9223bccdce0c9a05772f84ca6fc5606091
ZacharyWesterman <ZacharyWesterman@users.noreply.github.com>
2026-09-24 05:59:30 +0000

    deploy: 8f1c5a6db85b85b7e1dbe147af77ab0b28aafa03

--- search/all_16.js
+++ search/all_16.js
@@ -2,8 +2,7 @@ var searchData=
 [
   ['_7earray_0',['~array',['../classz_1_1core_1_1array.html#aa1a7259bdf173e04f1ab908d5327a65e',1,'z::core::array']]],
   ['_7edictionary_1',['~dictionary',['../classz_1_1util_1_1dictionary.html#a82c4b3c88bb90eb35e408075a188af67',1,'z::util::dictionary']]],
-  ['_7edirscan_2',['~dirscan',['../structz_1_1file_1_1dirscan.html#ad1144ef0e1c14c6cdcc0a231b1447719',1,'z::file::dirscan']]],
-  ['_7efilehandle_3',['~fileHandle',['../structz_1_1file_1_1fileHandle.html#a7908232c7278af2a397640da72b798ba',1,'z::file::fileHandle']]],
-  ['_7elibrary_4',['~library',['../classz_1_1file_1_1library.html#ac8c5743402e5f0ab3c50bb0f3deeb5eb',1,'z::file::library']]],
-  ['_7estring_5',['~string',['../classz_1_1core_1_1string.html#a63330bb4cd06265520bd062cffba446b',1,'z::core::string']]]
+  ['_7efilehandle_2',['~fileHandle',['../structz_1_1file_1_1fileHandle.html#a7908232c7278af2a397640da72b798ba',1,'z::file::fileHandle']]],
+  ['_7elibrary_3',['~library',['../classz_1_1file_1_1library.html#ac8c5743402e5f0ab3c50bb0f3deeb5eb',1,'z::file::library']]],
+  ['_7estring_4',['~string',['../classz_1_1core_1_1string.html#a63330bb4cd06265520bd062cffba446b',1,'z::core::string']]]
 ];
--- structz_1_1file_1_1dirscan.js
+++ structz_1_1file_1_1dirscan.js
@@ -1,6 +1,5 @@
 var structz_1_1file_1_1dirscan =
 [
-    [ "~dirscan", "structz_1_1file_1_1dirscan.html#ad1144ef0e1c14c6cdcc0a231b1447719", null ],
     [ "showAll", "structz_1_1file_1_1dirscan.html#af4bf7dfe48fa80e5acca8cf94686e59f", null ],
     [ "used", "structz_1_1file_1_1dirscan.html#ae5102e4aae44011371dc1ce7e866d73e", null ]
 ];
